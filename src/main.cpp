2fa9edec64c16a6e9e43482d2292ef88de407cc2
Animal Studio <61629960+AnimalStudioOfficial@users.noreply.github.com>
2023-01-08 11:34:27 -0500

    update

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{ 
+  char string[30];
+  cout << "Enter a string: ";
+  gets_s(string);       //input the string
+  return 0;
+}
