ecdb4e00765dae8568b033f04aa093a238975fd4
Animal Studio <61629960+AnimalStudioOfficial@users.noreply.github.com>
2023-01-08 11:55:33 -0500

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <string>
 
-int main(int argc, char *argv[])
-{
-	std::cout << "Hello world!" << std::endl;
+using namespace std;
+
+// val is passed by value
+pair<string, string> splitInTwo(string val) {
+    string arg;
+    string::size_type pos = val.find(':');
+    if(val.npos != pos) {
+        arg = val.substr(pos + 1);
+        val = val.substr(0, pos);
+    }
+    return make_pair(val, arg);
+}
+
+int main() {
+    string cmd = "HELLO:WORLD";
+
+    pair<string, string> pr = splitInTwo(cmd);
+
+    //cout << pr.first  << "\n";
+    //cout << pr.second << "\n";
+
+    return 0;
 }
