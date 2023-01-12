cd3a6ba707478bdb67d810b398800b89668b080d
Animal Studio <61629960+AnimalStudioOfficial@users.noreply.github.com>
2023-01-11 20:21:17 -0500

    UPDATE 0.6.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 
 
-
+#include "argparse.hpp"
 #include "core/parser.h"
 
 using namespace parser;
 using namespace std;
 
-
-
 int main() {
+
+
     parser_class parser_class;
     parser_class.parser_file("test.pie");
 
