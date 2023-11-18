556d9c9b59425a12f96b536e52b4980aac2218f2
Animal <61629960+AnimalStudioOfficial@users.noreply.github.com>
2023-11-18 17:30:16 -0500

    Update network.hpp

--- src/core/Network/network.hpp
+++ src/core/Network/network.hpp
@@ -7,11 +7,30 @@
 
 namespace Network {
 
+/**
+ * Callback function used by the libcurl library to write data into a string buffer.
+ *
+ * @param buffer A pointer to the buffer where the data should be written.
+ * @param size The size of each element to be written.
+ * @param nmemb The number of elements to be written.
+ * @param userp A pointer to the user-defined data.
+ *
+ * @return The total number of bytes written into the buffer.
+ */
 static size_t WriteCallback(void *buffer, size_t size, size_t nmemb, void *userp) {
   ((std::string*)userp)->append((char*)buffer, size * nmemb);
   return size * nmemb;
 }
 
+/**
+ * Retrieves the contents of the specified URL using a GET request.
+ *
+ * @param url the URL to retrieve the contents from
+ *
+ * @return the response body as a string
+ *
+ * @throws std::string an error message if the request fails
+ */
 std::string Network::get(std::string url) {
   CURL *curl = curl_easy_init();
   if (curl) {
@@ -30,9 +49,10 @@ std::string Network::get(std::string url) {
     } else {
       std::cerr << "curl error: " << curl_easy_strerror(res) << std::endl;
     }
+  } else {
+    std::cerr << "Failed to initialize curl" << std::endl;
   }
 
-  return 0;
 }
 
 }
